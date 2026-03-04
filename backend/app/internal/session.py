afba262cc1e5ea52ecde80bf2b2a717b1bb1d589
EDITH5607 <aadharshajith5607@gmail.com>
2026-03-05 03:36:04 +0530

    validation ,submission ,student summary route and project structure added

---
+++ backend/app/internal/session.py
@@ -0,0 +1,5 @@
+from .config import supabase
+
+def get_supabase():
+    print("SupaBase connection established Sucessfully!!")
+    return supabase
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+from datetime import date
+import uuid
+import uvicorn
+from routes import student
+
  
 app = FastAPI(title="Tractivity")
 
 @app.get("/")
 def read_root():
       return "welcome to Tractivity"
+
+
+app.include_router(student.app)
+
+if __name__ =="__main__":
+      uvicorn.run("main:app",host="127.0.0.1",port=8000,reload=True)
+
+
+
+
 
